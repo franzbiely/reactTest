1c717ec13e310a1447a36b58b932c6d5d0b01203
Francis Albores <mrthemetribe@gmail.com>
2024-09-12 18:14:15 +0800

    init

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Welcome: React.FC = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold mb-4">Welcome to My Next.js App!</h1>
+      <p className="text-xl text-gray-600">
+        This is a simple app built with Next.js and Tailwind CSS.
+      </p>
+    </div>
+  )
+}
+
+export default Welcome
